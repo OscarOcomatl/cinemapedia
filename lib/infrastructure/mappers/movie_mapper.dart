79294bf6367af05d505f74a39ef22c13c1ce55fb
OscarDev13 <oscarperezocomatldev@gmail.com>
2024-08-01 00:11:15 -0600

    Se definio la arquitectura, se implemento provider con riverpod y se obtuvieron las peliculas

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+    adult: moviedb.adult,
+    backdropPath: (moviedb.backdropPath != '')
+      ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+      : 'https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg',
+    genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+    id: moviedb.id,
+    originalLanguage: moviedb.originalLanguage,
+    originalTitle: moviedb.originalTitle,
+    overview: moviedb.overview,
+    popularity: moviedb.popularity,
+    posterPath: (moviedb.posterPath != '')
+    ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+    : 'no-poster',
+    releaseDate: moviedb.releaseDate,
+    title: moviedb.title,
+    video: moviedb.video,
+    voteAverage: moviedb.voteAverage,
+    voteCount: moviedb.voteCount
+  );
+}
---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:cinemapedia/domain/datasources/movie_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+
+
+  final MoviesDatasource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying();
+  }
+
+
+}
+
