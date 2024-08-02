456b5141fe858542acd7c4eae0881f4349110f4b
OscarDev13 <oscarperezocomatldev@gmail.com>
2024-08-02 00:20:05 -0600

    Se agrego card swiper de nowPlaying

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -35,15 +36,13 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
-    if(nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator());
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    // if(nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator());
+
+    return Column(
+      children: [
+        CustomAppbar(),
+        MoviesSlideshow(movies: nowPlayingMovies)
+      ]
     );
   }
 }
